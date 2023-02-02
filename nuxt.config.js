0c70caa60816c4206bb18eff2138b80aa000ac31
Limbo <1034935762@qq.com>
2023-02-02 17:05:38 +0800

    2023年2月2日17:05:38

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
     css: ["element-ui/lib/theme-chalk/index.css", "@/assets/styles/reset.scss"],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: ["@/plugins/element-ui"],
+    // plugins: ["@/plugins/element-ui"],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
